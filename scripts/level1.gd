4570767172e11954b542c5c14442a9e21afae307
noura <c22388271@mytudublin.ie>
2024-03-12 21:37:59 +0000

    better pause?

--- scripts/level1.gd
+++ scripts/level1.gd
@@ -9,10 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-		
-func _input(event):
-	if event.is_action_pressed("esc"):
+	if Input.is_action_just_pressed("esc"):
 		if isPaused:
 			print("***UNPAUSE***")
 			unpause()
@@ -22,12 +19,28 @@ func _input(event):
 	else:
 		pass
 		
+
+func _input(event):
+	pass
+	#if event.is_action_pressed("esc"):
+		#if isPaused:
+			#print("***UNPAUSE***")
+			#unpause()
+		#else:
+			#print("***PAUSE***")
+			#pause()
+	#else:
+		#pass
+		
+# stop game time when ESC pressed
+# using engine.time_scale because if use get_tree().pause = true then processing
+# will stop and can no longer detect input, meaning pause never be unpaused
 func pause():
 	isPaused = true
 	Engine.time_scale = 0
 	$CanvasLayer/Pause.show()
-	
 
+# start the time when ESC pressed again
 func unpause():
 	isPaused = false
 	Engine.time_scale = 1
