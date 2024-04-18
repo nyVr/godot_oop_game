f0c577e5e27a54395c70938cc591c1ccd023c75c
noura <124163358+nrn5@users.noreply.github.com>
2024-04-18 04:36:22 +0100

    animation queue

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -1,9 +1,23 @@
 extends Node3D
+# manually matching cutscene with mc animation
+var animqueue = ["cutsceneP1", "cutsceneP2"]
+var mcanimqueue = ["look_up", "[stop]"]
+
+# array vars for iteration
+var index = 0
+var maxindex = animqueue.size()
 
 func _ready():
-	$cutscene.play("cutsceneP1")
-	$mainchar/AnimationPlayer.play("look_up")
-	
+	play_nims()
 
+# queue for animations
 func play_nims():
-	pass
+	$cutscene.play(animqueue[index])
+	$mainchar/AnimationPlayer.play(mcanimqueue[index])
+
+## signal
+func _on_cutscene_animation_finished(anim_name):
+	index += 1
+	# reached end of animaiton queue
+	if index < maxindex:
+		play_nims()
