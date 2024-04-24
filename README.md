325875fe9466b7e34f523828d71190b01b97f28b
noura <124163358+nrn5@users.noreply.github.com>
2024-04-24 15:54:28 +0100

    pause volumes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Star Seekeer
+# Star Seeker
 
 Name: Noura Belkhatir
 Student Number: C22388271
--- scenes/UI/pause.gd
+++ scenes/UI/pause.gd
@@ -2,6 +2,10 @@ extends Control
 
 # pause func, pressing buttons
 
+func _ready():
+	$bg.show()
+	$MarginContainer.show()
+	$volumeSliders.hide()
 
 func _on_quit_pressed():
 	get_tree().quit()
@@ -9,3 +13,15 @@ func _on_quit_pressed():
 
 func _on_controls_pressed():
 	pass # Replace with function body.
+
+
+func _on_volume_pressed():
+	$bg.hide()
+	$MarginContainer.hide()
+	$volumeSliders.show()
+
+
+func _on_back_vol_pressed():
+	$volumeSliders.hide()
+	$bg.show()
+	$MarginContainer.show()
