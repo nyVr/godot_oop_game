2629ce70f8480388dd58eb1c8c392c5b319ec18e
noura <124163358+nrn5@users.noreply.github.com>
2024-04-24 14:23:31 +0100

    volume sliders

--- scenes/UI/optionsMenu.gd
+++ scenes/UI/optionsMenu.gd
@@ -1,10 +1,23 @@
 extends Control
 
 
+func _ready():
+	$volumeSliders.hide()
+	$sprite.show()
+	$MarginContainer.show()
+
 
 func _on_volume_pressed():
-	pass # Replace with function body.
+	$volumeSliders.show()
+	$sprite.hide()
+	$MarginContainer.hide()
 
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/UI/mainMenu.tscn")
+
+
+func _on_back_vol_pressed():
+	$volumeSliders.hide()
+	$sprite.show()
+	$MarginContainer.show()
