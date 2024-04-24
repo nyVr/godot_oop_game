eebd92c3526cc6dc9f0e452e8f5463ecd9586ed4
noura <124163358+nrn5@users.noreply.github.com>
2024-04-24 02:41:04 +0100

    tutorial

--- scenes/UI/tutorial.gd
+++ scenes/UI/tutorial.gd
@@ -5,18 +5,63 @@ extends Control
 # when prev clicked display prev page
 # when skip clicked skip to level
 
-var pages = [$page1, $page2, $page3, $page4]
+@onready var pages = [$page1, $page2, $page3, $page4, $page5]
+@onready var amt = pages.size()
+@onready var currIndex = 0
+
+func _ready():
+	update_btn_visibility()
 
 
+## BUTTONS PRESSED
 
 
 func _on_next_pressed():
-	pass # Replace with function body.
+	if currIndex < (amt-1):
+		currIndex += 1
+		update_btn_visibility()
 
 
 func _on_prev_pressed():
-	pass # Replace with function body.
+	if currIndex > 0:
+		currIndex -= 1
+		update_btn_visibility()
 
 
 func _on_skip_pressed():
-	pass # Replace with function body.
+	if currIndex < (amt-1):
+		get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+
+# 
+func update_btn_visibility():
+	# hide all btns 
+	$play.hide()
+	$next.hide()
+	$prev.hide()
+	$skip.hide()
+	
+	# show approproate btn
+	if currIndex == 0:
+		$next.show()
+		$skip.show()
+	elif currIndex == (amt - 1):
+		$prev.show()
+		$play.show()
+	else:
+		$next.show()
+		$prev.show()
+		$skip.show()
+	
+	# show curr page
+	for i in range(amt):
+		if i == currIndex:
+			pages[i].show()
+		else:
+			pages[i].hide()
+
+
